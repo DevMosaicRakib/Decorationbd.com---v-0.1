501ac2d635068dae9e29b1b16d6ae76c5e46c5c2
DevMosaicRakib <devmosaic17@gmail.com>
2025-07-09 02:22:52 +0600

    update msngr chat

--- frontend/src/components/MessengerChat.jsx
+++ frontend/src/components/MessengerChat.jsx
@@ -2,33 +2,34 @@ import { useEffect } from "react";
 
 const MessengerChat = () => {
   useEffect(() => {
-    const loadMessenger = () => {
-      window.fbAsyncInit = function () {
-        window.FB.init({
-          xfbml: true,
-          version: "v19.0",
-        });
-      };
-
-      const chatDiv = document.createElement("div");
-      chatDiv.id = "fb-customer-chat";
-      chatDiv.className = "fb-customerchat";
-      chatDiv.setAttribute("page_id", "463824586815253"); // আপনার Page ID
-      chatDiv.setAttribute("attribution", "biz_inbox");
-      document.body.appendChild(chatDiv);
-
-      const script = document.createElement("script");
-      script.src = "https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js";
-      script.async = true;
-      script.crossOrigin = "anonymous";
-      script.nonce = "abc";
-      document.body.appendChild(script);
+    window.fbAsyncInit = function () {
+      window.FB.init({
+        xfbml: true,
+        version: "v19.0",
+      });
     };
 
-    loadMessenger();
+
+    const chatDiv = document.createElement("div");
+    chatDiv.id = "fb-customer-chat";
+    chatDiv.className = "fb-customerchat";
+    chatDiv.setAttribute("page_id", "463824586815253"); 
+    chatDiv.setAttribute("attribution", "biz_inbox");
+    document.body.appendChild(chatDiv);
   }, []);
 
-  return <div id="fb-root"></div>;
+  return (
+    <>
+      <div id="fb-root"></div>
+
+      <script
+        async
+        defer
+        crossOrigin="anonymous"
+        src="https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js"
+      ></script>
+    </>
+  );
 };
 
 export default MessengerChat;
