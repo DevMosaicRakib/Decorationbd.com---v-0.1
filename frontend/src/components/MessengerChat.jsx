5b2a5ba7e4358af45fcc8751f4a8dc009701634f
DevMosaicRakib <devmosaic17@gmail.com>
2025-07-09 02:58:40 +0600

    update msngr chat1

--- frontend/src/components/MessengerChat.jsx
+++ frontend/src/components/MessengerChat.jsx
@@ -2,6 +2,7 @@ import { useEffect } from "react";
 
 const MessengerChat = () => {
   useEffect(() => {
+   
     window.fbAsyncInit = function () {
       window.FB.init({
         xfbml: true,
@@ -9,27 +10,36 @@ const MessengerChat = () => {
       });
     };
 
+    
+    if (!document.getElementById("fb-root")) {
+      const fbRoot = document.createElement("div");
+      fbRoot.id = "fb-root";
+      document.body.appendChild(fbRoot);
+    }
 
-    const chatDiv = document.createElement("div");
-    chatDiv.id = "fb-customer-chat";
-    chatDiv.className = "fb-customerchat";
-    chatDiv.setAttribute("page_id", "463824586815253"); 
-    chatDiv.setAttribute("attribution", "biz_inbox");
-    document.body.appendChild(chatDiv);
+    
+    if (!document.getElementById("fb-customer-chat")) {
+      const chatDiv = document.createElement("div");
+      chatDiv.id = "fb-customer-chat";
+      chatDiv.className = "fb-customerchat";
+      chatDiv.setAttribute("page_id", "463824586815253");
+      chatDiv.setAttribute("attribution", "biz_inbox");
+      document.body.appendChild(chatDiv);
+    }
+
+    
+    if (!document.getElementById("facebook-jssdk")) {
+      const script = document.createElement("script");
+      script.id = "facebook-jssdk";
+      script.src = "https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js";
+      script.async = true;
+      script.defer = true;
+      script.crossOrigin = "anonymous";
+      document.body.appendChild(script);
+    }
   }, []);
 
-  return (
-    <>
-      <div id="fb-root"></div>
-
-      <script
-        async
-        defer
-        crossOrigin="anonymous"
-        src="https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js"
-      ></script>
-    </>
-  );
+  return null; 
 };
 
 export default MessengerChat;
